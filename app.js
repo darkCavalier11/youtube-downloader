89204169a532502de377d25db879f37a911b2b6a
sumit <sumit.pradhan65@gmail.com>
2020-07-22 18:50:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+ 
+const info = async function(){
+  const data = await ytdl.getBasicInfo('https://www.youtube.com/watch?v=7JJfJgyHYwU')
+  console.log(data)
+}
+info()
