71ddeac44ed7a5de1662adf9e14d655d5d0c0bbd
sumit <sumit.pradhan65@gmail.com>
2020-07-23 10:25:54 +0530

    final bug fix

--- app.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const ytdl = require('ytdl-core');
- 
-const info = async function(){
-  const data = await ytdl.getBasicInfo('https://www.youtube.com/watch?v=7JJfJgyHYwU')
-  console.log(data.videoDetails.title)
-}
-info()
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const ytdl = require('ytdl-core')
 const app = express()
-const PORT = process.env.PORT || 5989
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('./public'))
 
